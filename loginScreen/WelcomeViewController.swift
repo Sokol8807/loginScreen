1f353006d467fe1156c9608efc3dd70a94930c07
Sokol8807 <sokol8807@gmail.com>
2022-08-12 23:21:35 +0300

    накидал общий каркас двух вьюх

---
+++ loginScreen/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  loginScreen
+//
+//  Created by Ilya Sokolov on 12.08.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
